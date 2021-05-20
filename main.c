772e3a4d9cefb42b068b460de157c91272584851
JCKeep <75779357+JCKeep@users.noreply.github.com>
2021-05-21 07:39:50 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+
+#include "BTree.h"
+#include "btreedef.h"
+
+int main()
+{
+	b_tree a;
+	char file[20];
+	
+	scanf("%s", file);
+	
+	B_TREE_CREATE(&a, file);
+
+
+	b_tree b;
+	
+	B_TREE_LOAD(&b, file);
+	
+	
+
+	printf("filename:%s\tfd:%d\troot:%ld\n", a.filename, a.fd, a.root);
+	printf("filename:%s\tfd:%d\troot:%ld\n", b.filename, b.fd, b.root);
+	
+	int i;
+	for (i = 0; i < 100; ++i) {
+		B_TREE_INSERT(&b, i);
+	}
+	
+	
+	for (i = 0; i < 1000; ++i) {
+		BOOL flag = B_TREE_SEARCH(&b, b.root, i);
+		if (flag == TRUE)
+			printf("\033[1;42mtrue\033[0m\n");
+		else
+			printf("\033[1;41mfalse\033[0m\n");
+	}
+	
+		
+	
+	/*for (i = 0; i < 10; ++i) {
+		int tmp;
+		scanf("%d", &tmp);
+		BOOL flag = B_TREE_SEARCH(&b, b.root, tmp);
+		if (flag == TRUE)
+			printf("\033[1;42mtrue\033[0m\n");
+		else
+			printf("\033[1;41mfalse\033[0m\n");
+	}*/
+	
+	
+	
+	/*for (i = 0; i < 10; ++i) {
+		int tmp;
+		scanf("%d", &tmp);
+		BOOL flag = B_TREE_SEARCH(&b, b.root, tmp);
+		if (flag == TRUE)
+			printf("\033[1;42mtrue\033[0m\n");
+		else
+			printf("\033[1;41mfalse\033[0m\n");
+	}*/
+	
+	B_TREE_LOAD_CLOSE(&b);
+	
+	close(b.fd);
+	
+
+	return 0;
+}
